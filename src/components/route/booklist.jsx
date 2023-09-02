322009ece759955143b2259a0fcbbc4b19e6e8a0
misikira <misikirteka@gmail.com>
2023-09-02 20:29:39 +0300

    add book component

--- src/components/route/booklist.jsx
+++ src/components/route/booklist.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import '../css/bookStyle.css';
 
 const BookList = (prop) => {
+  <book/>
   const { book, deleteBook } = prop;
   return (
     <li key={book.id} className="book-list">
