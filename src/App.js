555ba9241660369bb338184df449cb10b80c4123
misikira <misikirteka@gmail.com>
2023-09-02 20:50:05 +0300

    fix liters issues

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import Navbar from './components/navbar';
-import Book from './components/route/book';
 import BookPage from './components/route/books';
 import CategoryPage from './components/route/categories';
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
-      <Book  />
+      <Navbar/>
       <section>
         <Routes>
           <Route path="/" element={<BookPage />} />
