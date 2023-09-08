4f8eb4f43eabb209ca778f164bb18f53308bf1d8
misikira <misikirteka@gmail.com>
2023-09-08 13:58:52 +0300

    fix liters error

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import Navbar from "./components/navbar";
+import BookPage from "./components/route/books";
+import CategoryPage from "./components/route/categories";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<Navbar />
+			<section>
+				<Routes>
+					<Route path="/" element={<BookPage />} />
+					<Route path="/categories" element={<CategoryPage />} />
+				</Routes>
+			</section>
+		</div>
+	);
 }
 
 export default App;
