58960a86eedbe4f148f5c2c5b70cd063a6b38a8f
misikira <misikirteka@gmail.com>
2023-09-08 16:39:33 +0300

    fix liters error

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 // import ReactDOM from 'react-dom/client';
-import ReactDOM from 'react-dom';
+import createRoot from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import { Provider } from 'react-redux';
@@ -8,7 +8,7 @@ import store from './redux/store';
 import App from './App';
 // import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
+createRoot.render(
   <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter>
