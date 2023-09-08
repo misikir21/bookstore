d5a252ded48a3c0612072a9d1324800e2e9fa0bf
misikira <misikirteka@gmail.com>
2023-09-08 14:10:17 +0300

    fix liters error

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import "./App.css";
-import { Route, Routes } from "react-router-dom";
-import Navbar from "./components/navbar";
-import BookPage from "./components/route/books";
-import CategoryPage from "./components/route/categories";
+import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Navbar from './components/navbar';
+import BookPage from './components/route/books';
+import CategoryPage from './components/route/categories';
 
 function App() {
 	return (
