880b4069c580e19b6a363fcd6856bb98eb643550
misikira <misikirteka@gmail.com>
2023-09-08 14:12:51 +0300

    fix liters error

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
-import Navbar from './components/navbar';
-import BookPage from './components/route/books';
-import CategoryPage from './components/route/categories';
-
+import Navbar from './components/navbar.js';
+import BookPage from './components/route/books.js';
+import CategoryPage from './components/route/categories.js';
 function App() {
-	return (
+return (
 		<div className="App">
 			<Navbar />
 			<section>
