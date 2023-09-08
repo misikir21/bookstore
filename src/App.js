162a09e23e6ecd66959b9516f862ccc847b9ec30
misikira <misikirteka@gmail.com>
2023-09-08 14:39:29 +0300

    add

--- src/App.js
+++ src/App.js
@@ -4,18 +4,17 @@ import Navbar from './components/navbar.js';
 import BookPage from './components/route/books.js';
 import CategoryPage from './components/route/categories.js';
 
-  function App() {
-return (
-		<div className="App">
-			<Navbar />
-			<section>
-				<Routes>
-					<Route path="/" element={<BookPage />} />
-					<Route path="/categories" element={<CategoryPage />} />
-				</Routes>
-			</section>
-		</div>
-	);
+function App() {
+  return (
+  <div className="App">
+  <Navbar />
+<section>
+  <Routes>
+  <Route path="/" element={<BookPage />} />
+  <Route path="/categories" element={<CategoryPage />} />
+  </Routes>
+  </section>
+  </div>
+);
 }
-
 export default App;
