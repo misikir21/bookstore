e65f7b6e932a5e25630af47d980a1519c19688a1
misikira <misikirteka@gmail.com>
2023-09-08 15:20:47 +0300

    fix liters error

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
           <Route path="/" element={<BookPage />} />
           <Route path="/categories" element={<CategoryPage />} />
         </Routes>
-    </section>
+      </section>
     </div>
   );
 }
