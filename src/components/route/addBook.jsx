0ac4704072f6775a568e7e36029946cb4e603edc
misikira <misikirteka@gmail.com>
2023-09-19 16:33:08 +0300

    fix all errors

--- src/components/route/addBook.jsx
+++ src/components/route/addBook.jsx
@@ -1,8 +1,9 @@
-
-import React, { useState } from 'react';
-import PropTypes from 'prop-types';
-import '../css/bookStyle.css';
+import { useDispatch } from 'react-redux';
+import { useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
+import { addBookToApi, addBook } from '../../redux/books/booksSlice';
 import '../css/addBooks.css';
+import '@fontsource/roboto-slab/latin-400.css';
 
 const AddBook = () => {
   const dispatch = useDispatch();
@@ -23,6 +24,26 @@ const AddBook = () => {
   };
 
   return (
-    <div className="book-form">
+    <div className="FormContianer-all">
+      <div className="FormContianer">
+        <div className="title">
+          <h3>ADD NEW BOOK</h3>
+        </div>
 
+        <form className="form">
+          <div className="input-container">
+            <input value={title} type="text" id="title" placeholder="Book Title Here..." onChange={(e) => setTitle(e.target.value)} />
+          </div>
+          <div className="input-container">
+            <input value={author} type="text" id="name" placeholder="Author Name Here..." onChange={(e) => setAuthor(e.target.value)} />
+          </div>
+          <div className="input-container">
+            <button type="button" value="AddNew" onClick={handleSubmit}> ADD BOOK </button>
+          </div>
+        </form>
+      </div>
+
+    </div>
+  );
+};
 export default AddBook;
